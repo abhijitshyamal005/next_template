b06286ae049667e6b4e05c163243db3b4ccd0647
Abhijit Shyamal <abhijitshyamal005@gmail.com>
2025-03-30 12:34:07 +0530

    update

--- src/app/components/Header/index.tsx
+++ src/app/components/Header/index.tsx
@@ -1,21 +1,9 @@
-import { Button } from "@/components/ui/button";
+import React from 'react'
 
-const Header = () => {
+const index = () => {
   return (
-    <div className="bg-black sticky top-0 text-white py-3 border-b border-[#262626]">
-      <div className="max-w-screen-2xl mx-auto flex justify-between items-center px-10">
-        <div className="text-lg font-normal">Magic UI</div>
-        <div className="flex space-x-3">
-          <Button variant="ghost" className="text-white text-md font-normal">
-            Log in
-          </Button>
-          <Button className="bg-[#262626] text-white px-4 py-1.5 rounded-lg text-sm">
-            Sign up
-          </Button>
-        </div>
-      </div>
-    </div>
-  );
-};
+    <div>index</div>
+  )
+}
 
-export default Header;
+export default index
